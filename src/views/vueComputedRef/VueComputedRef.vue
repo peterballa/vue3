277015d5cae52523fcd8beb94ddd46cd3a251d83
Balla Péter <balla.peter.it@marso.hu>
2022-09-27 16:08:35 +0200

    ComputedRef and Lifecycle Hooks page modified.

--- src/views/vueComputedRef/VueComputedRef.vue
+++ src/views/vueComputedRef/VueComputedRef.vue
@@ -1,7 +1,28 @@
 <template>
-  TODO
+  <div class="text-center">Give the shipping address</div>
+  <div class="text-center">
+    <input v-model="zipCode" class="border border-2 mt-3 rounded p-3" placeholder="Zip Code"/>
+  </div>
+  <div class="text-center">
+    <input v-model="city" class="border border-2 mt-3 rounded p-3" placeholder="City"/>
+  </div>
+  <div class="text-center">
+    <input v-model="street" class="border border-2 mt-3 rounded p-3" placeholder="Street"/>
+  </div>
+  <div class="text-center mt-3">
+    {{shippingAddress}}
+  </div>
 </template>
 
 <script lang="ts" setup>
 
+import {computed, ComputedRef, ref, Ref} from "vue";
+
+const zipCode: Ref<string> = ref('');
+const city: Ref<string> = ref('');
+const street: Ref<string> = ref('');
+
+const shippingAddress: ComputedRef<string> = computed(() => {
+  return `${zipCode.value} ${city.value} ${street.value}`;
+})
 </script>
--- src/views/vueLifecycleHooks/VueLifecycleHooks.vue
+++ src/views/vueLifecycleHooks/VueLifecycleHooks.vue
@@ -1,7 +1,37 @@
 <template>
-  <h1>Check the console!</h1>
+  <div class="text-center">Check the console!</div>
+  <div class="text-center">
+    <input v-model="name" class="border border-2 mt-3 rounded p-3" placeholder="Your name"/>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import {onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref, Ref} from "vue";
 
+const name: Ref<string> = ref('');
+
+onBeforeMount(() => {
+  //load async data
+  console.log('onBeforeMount');
+})
+
+onMounted(() => {
+  console.log('onMounted');
+});
+
+onBeforeUpdate(() => {
+  console.log('beforeUpdate');
+})
+
+onUpdated(() => {
+  console.log('updated')
+})
+
+onBeforeUnmount(() => {
+  console.log('beforeUnmount');
+});
+
+onUnmounted(() => {
+  console.log('unmounted');
+});
 </script>
